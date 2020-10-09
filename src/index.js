5e1e61c05ec5debee69ac2e97aec7460f62be55d
Kateryna Chovhan <chovganek@gmail.com>
2020-10-09 19:56:22 +0300

    feat: add result reverse int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+    let num = n;
+    if ( num < 0) n = n*(-1);
+    console.log(n);
+    let hundred = (n % 10) *100;
+    console.log(hundred);
+    let units = Math.trunc(n / 100);
+    console.log(units);
+    let decimal = Math.trunc((n % 100) / 10)*10;
+    console.log(decimal);
+    if (hundred === 0) return decimal + units;
+    return hundred + decimal + units;
 }
